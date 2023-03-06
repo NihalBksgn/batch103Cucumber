ed1ae41bcb21c58f6f2bd72fc85bd0ce400875fd
Nihal Bakisgan <nihalkaratasbakisgan@gmail.com>
2023-03-06 12:24:34 +0100

    06.03

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features = "./src/test/resources/features",//features folder path
         glue = {"stepdefinitions","hooks"},   //stepdefinitions path
-        tags = "@testrunner",
+        tags = "@failed_scenario",
         dryRun = false
 )
 public class Runner {
